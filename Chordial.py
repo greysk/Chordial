16d53fd6e33cc02ef008f822e8f1c34995ba67f2
coal-fingers <80172010+coal-fingers@users.noreply.github.com>
2022-02-07 20:32:31 -0500

    Update Chordial.py
    Attempting to fix an error where app crashes when calling the run function after a file is opened with the default program. Also looking to find a method to determine a file types default programs path to plug into subprocess.run() instead of using os.startfile()

--- Chordial.py
+++ Chordial.py
@@ -1,43 +1,50 @@
-import pygame, pygame.mixer, random, os
+import pygame, pygame.mixer, random, os, threading, subprocess
 from tkinter import *
 from tkinter import ttk
 from pygame import *
 from pathlib import Path
 
-Chordial = (r'.\Chord Templates\Midi Progressions')
+Chordial = Path('.\Midi Progressions')
 os.chdir(Chordial)
 
 pygame.init()
 pygame.mixer.init()
 
-class Chordial:
-    #Calls the random module on the current working directory to return a '.mid' file
-    def run(self): 
-        mysound = random.choice(os.listdir())
-        self.file_label.config(text = mysound)
-        pygame.mixer.music.load(mysound)
+class program_body:
+
+    #Selects and plays a random '.mid' file from the current working directory as well as updates the label
+    def run(self, *args): 
+        self.mysound = random.choice(os.listdir())
+        self.file_label.config(text = self.mysound)
+        pygame.mixer.music.load(self.mysound)
         pygame.mixer.music.play()
-        global file_to_open
-        file_to_open = mysound
-        return mysound;
+        self.file_to_open = self.mysound
+        self.file_label.config(text = self.mysound.replace('.mid',''))
+        return self.file_label
 
     #Opens the chord progression in a new instance of the default program specified for '.mid' files
-    def open(self):
-        os.startfile(file_to_open)  
+    def open(self, args=(1)):
+        #self.key = b'self.file_to_open'
+        #self.value = os.getenvb(self.key)
+        #print(self.value)
+        #subprocess.run(['J:\Studio One\Studio One 5\Studio One.exe', self.file_to_open])
+        os.startfile(self.file_to_open)
+        #x = threading.Thread(target = open, args=(1,))
+        #x.start()
      
-    #Used to replay the current audio file    
-    def replay(self):
-        pygame.mixer.music.load(file_to_open)
+        #Used to replay the current audio file    
+    def replay(self, *args):
+        pygame.mixer.music.load(self.file_to_open)
         pygame.mixer.music.play()
         return
 
+        #main instance of the program 
     def __init__(self):
         self.root = Tk()
         self.root.title("Chordial")
         self.frm = ttk.Frame(self.root, padding = 10)
         self.frm.grid()
         ttk.Label(self.frm, text = "Chord Progression Suggester:", font = "Ubuntu").grid(column = 0, row = 0)
-
         ttk.Button(self.frm, text = "Randomize", command = self.run).grid(column = 1, row = 0)       
         ttk.Button(self.frm, text = "Open in Default", command = self.open).grid(column = 2, row = 0)
         ttk.Button(self.frm, text = "Exit", command = self.root.destroy).grid(column = 3, row = 0)     
@@ -47,4 +54,4 @@ class Chordial:
 
         self.root.mainloop()
 
-Chordial()
+program_body()
