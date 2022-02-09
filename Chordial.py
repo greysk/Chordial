bc1591fef18cc815eee896d49607567f69093bf6
Grae T <83403671+greysk@users.noreply.github.com>
2022-02-09 05:46:44 -0500

    Fix style to match PEP 8
    Rules from PEP8 matching Changes made (https://pep8.org/)
    
    For import statement changes (New lines 1 to 7):
    https://pep8.org/#imports
    - Imports should usually be on separate lines unless used with from <module> import <object> (No: import os, sys. Ok: from subprocess import Popen, PIPE)
    - Group imports in following order with a blank line between each group: (1) standard library imports (2) related third party imports (3) local applications/library specific imports.
    - Wildcard imports (from <module> import *) should be avoided, as they make it unclear which names are present in the namespace, confusing both readers and many automated tools.
    
    Capitalization (New line 9, 16, 61):
    https://pep8.org/#class-names
    - Class names should normally use the CapWords convention
    https://pep8.org/#constants
    - Constants are usually defined on a module level and written in all capital letters with underscores separating words.
    
    Comments (New lines 18, 28, 30 to 33. 35 to 36, 38, 44):
    https://pep8.org/#comments
    - Each line of a block comment starts with a # and a single space (unless it is indented text inside the comment)
    
    Added additional blank line before and after class ProgramBody:
    https://pep8.org/#blank-lines
    - Surround top-level function and class definitions with two blank lines. (Already met: Method definitions inside a class are surrounded by a single blank line.)
    
    Breaking up long lines of code (New lines 16 to 17, 27 to 28, 44 to 53):
    - Limit all lines to a maximum of 79 characters. The preferred way of wrapping long lines is by using Python’s implied line continuation inside parentheses, brackets and braces. -https://pep8.org/#maximum-line-length
     
    Spaces remove around keyword argument value (Old Lines 17, 21, 44, 46 to 52)
    https://pep8.org/#other-recommendations
    - Don’t use spaces around the = sign when used to indicate a keyword argument or a default parameter value.
    
    Other spacing:
    https://pep8.org/#other-recommendations
    - Avoid trailing whitespace anywhere. Because it’s usually invisible, it can be confusing

--- Chordial.py
+++ Chordial.py
@@ -1,56 +1,60 @@
-import pygame, random, os
+import pygame
+import random
+import os
 import tkinter as tk
 from tkinter import ttk
 from pathlib import Path
 
-Chordial = Path('.\Midi Progressions')
+Chordial = Path('./Midi Progressions')
 os.chdir(Chordial)
 
 pygame.init()
 pygame.mixer.init()
 
-class program_body:
 
-    #Selects and plays a random '.mid' file from the current working directory as well as updates the label
-    def run(self, *args): 
+class program_body:
+    # Selects and plays a random '.mid' file from the current working
+    # directory as well as updates the label
+    def run(self, *args):
         self.mysound = random.choice(os.listdir())
-        self.file_label.config(text = self.mysound)
+        self.file_label.config(text=self.mysound)
         pygame.mixer.music.load(self.mysound)
         pygame.mixer.music.play()
         self.file_to_open = self.mysound
-        self.file_label.config(text = self.mysound.replace('.mid',''))
+        self.file_label.config(text=self.mysound.replace('.mid', ''))
         return self.file_label
 
-    #Opens the chord progression in a new instance of the default program specified for '.mid' files
+    # Opens the chord progression in a new instance of the default program
+    # specified for '.mid' files
     def open(self, args=(1)):
-        #self.key = b'self.file_to_open'
-        #self.value = os.getenvb(self.key)
-        #print(self.value)
-        #subprocess.run(['J:\Studio One\Studio One 5\Studio One.exe', self.file_to_open])
         os.startfile(self.file_to_open)
-        #x = threading.Thread(target = open, args=(1,))
-        #x.start()
-     
-        #Used to replay the current audio file    
+
+    # Used to replay the current audio file
     def replay(self, *args):
         pygame.mixer.music.load(self.file_to_open)
         pygame.mixer.music.play()
         return
 
-        #main instance of the program 
+    # main instance of the program
     def __init__(self):
         self.root = tk.Tk()
         self.root.title("Chordial")
-        self.frm = ttk.Frame(self.root, padding = 10)
+        self.frm = ttk.Frame(self.root, padding=10)
         self.frm.grid()
-        ttk.Label(self.frm, text = "Chord Progression Suggester:", font = "Ubuntu").grid(column = 0, row = 0)
-        ttk.Button(self.frm, text = "Randomize", command = self.run).grid(column = 1, row = 0)       
-        ttk.Button(self.frm, text = "Open in Default", command = self.open).grid(column = 2, row = 0)
-        ttk.Button(self.frm, text = "Exit", command = self.root.destroy).grid(column = 3, row = 0)     
-        ttk.Button(self.frm, text = "Play Again", command = self.replay).grid(column = 4, row = 0)
-        self.file_label = tk.Label(self.root, text = "file name", font = 'Ubuntu')
-        self.file_label.grid(column = 0, row = 1)
+        ttk.Label(self.frm, text="Chord Progression Suggester:", font="Ubuntu"
+                  ).grid(column=0, row=0)
+        ttk.Button(self.frm, text="Randomize", command=self.run).grid(column=1,
+                                                                      row=0)
+        ttk.Button(self.frm, text="Open in Default", command=self.open
+                   ).grid(column=2, row=0)
+        ttk.Button(self.frm, text="Exit", command=self.root.destroy
+                   ).grid(column=3, row=0)
+        ttk.Button(self.frm, text="Play Again", command=self.replay
+                   ).grid(column=4, row=0)
+        self.file_label = tk.Label(self.root, text="file name", font='Ubuntu')
+        self.file_label.grid(column=0, row=1)
 
         self.root.mainloop()
+
 
 program_body()
