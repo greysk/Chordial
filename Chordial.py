c1bf94bd94c8a022615887376e4f9bdc94c998e0
Grae T <83403671+greysk@users.noreply.github.com>
2022-02-10 04:34:58 -0500

    Fix mixed bugs & Remove trailing space

--- Chordial.py
+++ Chordial.py
@@ -12,49 +12,50 @@ CHORDIAL = Path(__file__).parents[1] / 'Midi Progressions'
 
 
 class ProgramBody(ttk.Frame):
-    def __init__(self, midi_directory, title='Chordial'):
+    def __init__(self, midi_dir, title='Chordial'):
         """
         Sets up pygame and Tkinter for Chordial: chord progression suggester.
-        
-        With Chordial you can select and play random .mid from chords_dir.        
+
+        With Chordial you can select and play random .mid from chords_dir.
         It also allows you to conveniently open a selected .mid file using the
         default program for .mid files.
-        
-        Chordial program can be run by calling mainloop() on an instance of this class.
-  
+
+        Chordial program can be run by calling mainloop() on an instance of
+        this class.
+
         Args:
-            chords_dir (Pathlike):
-                Path to directory containing chord progression .mid file.
-            title (str, optional): Title for root tk window. Defaults to 'Chordial'.
+            chords_dir (Pathlike): Path to directory containing chord
+                progression .mid file.
+            title (str, optional): Title for root tk window.
+                Defaults to 'Chordial'.
         """
         super().__init__(master=tk.Tk(), padding=10)
-       
+
         # All .mid files in Midi Progressions.
         self.sound_files = list(Path(midi_dir).glob('*.mid'))
-        
+
         # Set by self.select_file() to hold current chord progression.
         self.file_to_open = None
         # Used in self.select_file() to automatically update Frame label.
         self.file_label = StringVar()
-        
+
         # Set up basics for Tkinter.
-        self.root = root
-        self.root.title(title)
+        self.master.title(title)
         self.grid()
         self._create_widgets()
-        
+
         # Initialize pygame.music needed by self.run() and self.replay()
         pygame.mixer.init()
-    
+
     def _create_widgets(self):
         """Set up Tkinter window labels and buttons.
         """
         ttk.Label(self, text="Chord Progression Suggester:", font="Ubuntu"
                   ).grid(column=0, row=0)
         # Label for chord progression filename.
-        tk.Label(self.root, text="file name", textvariable=self.file_label,
+        tk.Label(self.master, text="file name", textvariable=self.file_label,
                  font='Ubuntu').grid(column=0, row=1)
-        
+
         # Randomize button - selects current sound.
         ttk.Button(self, text="Randomize", command=self.run
                    ).grid(column=1, row=0)
@@ -70,18 +71,20 @@ class ProgramBody(ttk.Frame):
 
     def select_file(self):
         """Select a random .mid file from chords_dir. Update filename label.
+
+        Sets self.file_to_open.
         """
         self.file_to_open = random.choice(self.sound_files)
         # Set current sound label in Tkinter window
-        self.file_label.set(self.file_to_open)
-        
+        self.file_label.set(self.file_to_open.stem)
+
     def run(self):
         """Play random sound from chords_dir and update filename label.
-        
+
         Uses self.select_file().
         """
         self.select_file()
-        pygame.mixer.music.load(mysound)
+        pygame.mixer.music.load(self.file_to_open)
         pygame.mixer.music.play()
 
     def open(self):
@@ -95,18 +98,18 @@ class ProgramBody(ttk.Frame):
         """
         if self.file_to_open is not None:
             pygame.mixer.music.play()
-            
+
     def remove(self):
         """Remove current chord progression from suggestions list.
-        
+
         Does not delete file or affect filesystem. And will not apply
         to any future runs of the Chordial program.
         """
         if self.file_to_open is not None:
-            self.sound_files.remove(self.mysound)
+            self.sound_files.remove(self.file_to_open)
 
 
-if __name__ == '__main__'':
-    chordial_app = ProgramBody(midi_directory=CHORDIAL)
+if __name__ == '__main__':
+    chordial_app = ProgramBody(midi_dir=CHORDIAL)
     # Run cordial application loop.
     chordial_app.mainloop()
